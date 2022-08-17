ad68bdb383a72e322b1f3f297c1878ef9462bc19
GPinheiroLemos <guilemos0@gmail.com>
2022-08-17 20:12:58 -0300

    chi router cors

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module techTrash
 go 1.18
 
 require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/cors v1.2.1
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/gorilla/handlers v1.5.1
-	github.com/gorilla/mux v1.8.0
 	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8
 )
 
-require github.com/felixge/httpsnoop v1.0.3 // indirect
+require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,31 +1,19 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
 	"techTrash/connection"
 	"techTrash/controllers"
-	"techTrash/user"
 
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func main() {
 
-	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
-	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
-	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
-
 	connection.MysqlConnect()
-	router := mux.NewRouter()
-	router.HandleFunc("/lixeira", controllers.GetLixeira).Methods("GET")
-	router.HandleFunc("/loglixeira", controllers.GetLog).Methods("GET")
-	router.HandleFunc("/lixeira", controllers.PostLixeira).Methods("POST")
-	router.HandleFunc("/loglixeira", controllers.PostLog).Methods("POST")
-	router.HandleFunc("/cadastrarusuario", user.NewUser).Methods("POST")
-	router.HandleFunc("/autenticarusuario", user.AuthUser).Methods("POST")
-	log.Print("Running at port :8000")
-	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
+	router := chi.NewRouter()
+	router.Use(cors.AllowAll().Handler)
+	router.Get("/lixeira", controllers.GetLixeira)
+	http.ListenAndServe(":8000", router)
 }
